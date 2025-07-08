2bf9d7094ab4b4bf470cc13686be44866142cbb2
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-08 20:39:26 +0100

    Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,15 @@ const Projects = ()=> {
                 <li>
                     <strong>project-link</strong> - E-commerce website / App
                 </li>
+                <li>
+                    <strong>project-link</strong> - Visitor's management system
+                </li>
+                <li>
+                    <strong>project-link</strong> - Banking App / system
+                </li>
             </ul>
         </section>
-    )
+    );
 }
+
+export default Projects;
