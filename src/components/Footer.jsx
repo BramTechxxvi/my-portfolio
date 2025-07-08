ba02829fce7688abc6da3740f6ab6aee4a5d4945
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-08 23:42:22 +0100

    app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = ()=> {
     return (
-        <footer style={{ textAlign: 'center', padding: "1rem", background: "#121212"}}>
+        <footer style={{ textAlign: 'center', padding: "1rem", background: "#121212", color: "#e0e0e0" }}>
             <p>&copy; {new Date().getFullYear()} Ibrahim Ibrahim</p>
         </footer>
     );
