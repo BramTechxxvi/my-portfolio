e7d410aa4dca2630ba853f15d4f20d741683e9e6
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-08 18:59:38 +0100

    new

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = ()=> {
+    return (
+        <header className="header">
+            <div className="header__container">
+                <h1 className="header__title">Ibrahim Ibrahim</h1>
+                <p className="header__subtitle">Web Developer</p>
+            </div>
+
+        </header>
+    )
+}
