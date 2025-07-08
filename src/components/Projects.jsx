3cb3a904da62883130ec31180719da5777db1825
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-08 20:35:39 +0100

    About

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+const Projects = ()=> {
+    return (
+        <section>
+            <h2>Projects</h2>
+            <ul>
+                <li>
+                    <strong>project-link</strong> - E-commerce website / App
+                </li>
+            </ul>
+        </section>
+    )
+}
