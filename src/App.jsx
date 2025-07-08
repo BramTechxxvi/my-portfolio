7b38b1ea8171c971059cb2c0b8651165148a7640
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-08 19:56:20 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,16 @@
    import React from 'react';
-   import './App.css';
+   import Header from './components/Header';
 
    function App() {
      return (
        <div className="App">
-         <header className="App-header">
-           <h1>Your Name</h1>
-           <h2>Web Developer</h2>
-           <img src="" alt="Profile" className="profile-pic" />
-         </header>
-         <main>
-           <section>
-             <h3>About Me</h3>
-             <p>
-               Write a brief introduction about yourself, your skills, and your interests.
-             </p>
-           </section>
-           <section>
-             <h3>Projects</h3>
-             <ul>
-               <li>
-                 <a href="">Project 1</a> - Description of project 1.
-               </li>
-               <li>
-                 <a href="">Project 2</a> - Description of project 2.
-               </li>
-               <li>
-                 <a href="">Project 3</a> - Description of project 3.
-               </li>
-             </ul>
-           </section>
-           <section>
-             <h3>Contact</h3>
-             <p>Email: your.email@example.com</p>
-           </section>
-         </main>
+        <Header />
+        <main>
+
+        </main>
+        <footer>
+          
+        </footer>
        </div>
      );
    }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = ()=> {
         <header className="header">
             <section className="header__container">
                 <h1>Ibrahim Ibrahim Babatunde</h1>
-                <p>Backend Software Enginner / Tech Enthusiast</p>
+                <p>Backend Software Engineer / Tech Enthusiast</p>
             </section>
             <img src="" alt="Ibrahim" className="profile-image" />
         </header>
