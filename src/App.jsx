1caa473074b15497463b54eb0248d2defed5b807
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-09 11:24:34 +0100

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer';
 import Contact from './components/Contact';
 import Home from './Home';
 import './App.css';
@@ -14,7 +13,6 @@ import './App.css';
               <Route path="/" element={<Home />} />
               <Route path="/contact" element={<Contact />} />
             </Routes>
-            <Footer />
           </div>
         </Router>
      );
