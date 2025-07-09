b0148dc8aef69bd8b064e762c6f2f2ea2824edee
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-09 11:34:15 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Contact from './components/Contact';
 import Home from './Home';
-import './App.css';
+// import './App.css';
 
    function App() {
      return (
         <Router>
-          <div className="App">
-            <div className="overlay"></div>
-            <Routes>
+          <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/contact" element={<Contact />} />
             </Routes>
-          </div>
         </Router>
      );
    }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,9 @@ const Header = ()=> {
     return (
         <header className="header">
             <section className="headerContainer">
-                <h1>Ibrahim Ibrahim Babatunde</h1>
-                <p>Back-End Software Engineer / Tech Enthusiast</p>
+                <h6>Open to work</h6>
+                <button>Download CV</button>
             </section>
-            <img src="/android-chrome-192x192.png" alt="Ibrahim" className="profile-image" />
         </header>
     );
 }
