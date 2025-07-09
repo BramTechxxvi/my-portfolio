f3887328793513ac1dafccaa6b1ede9506d6133f
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-09 15:24:49 +0100

    home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "./Home.css"
 
 const Home = () => {
     return (
         <div className="Home">
             <Header />
-            <main>
+            <main className="mainContainer">
                 <h2>BACK-END SOFTWARE ENGINEER / TECH ENTHUSIAST</h2>
                 <h1>Ibrahim Ibrahim Babatunde</h1>
             </main>
