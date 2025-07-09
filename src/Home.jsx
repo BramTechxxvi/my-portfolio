66469485ae0198c57bb7df8ad331a441619ecf7b
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-09 11:09:25 +0100

    home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,9 @@
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Header from "./components/Header";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 const Home = () => {
     return (
