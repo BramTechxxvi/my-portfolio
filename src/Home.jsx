44f2ef3b0ada54b3bf097592d7b2eec9fdd3c5c3
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-11 15:56:50 +0100

    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -17,13 +17,13 @@ const Home = () => {
                         <h2>BACK-END/SOFTWARE ENGINEER</h2>
                         <h1>Ibrahim Ibrahim</h1>
                     </div>
+                    
                     <div className="info">
                         <h4 ><FiMail />  bramtechxxvi@gmail.com</h4>
                         <h4><FiPhone/>  +234-701-717-5172</h4>
                         <h4><FiLinkedin />  my-linkedIn</h4>
                         <h4><FiMapPin />   Lagos, Nigeria</h4>
                         <h4><FiGithub />   my-gitHubLink</h4>
-
                     </div>
 
                 </div>
