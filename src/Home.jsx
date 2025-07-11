59cd8352dfae73178e80508f98755f27fdf26444
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-11 14:34:03 +0100

    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -19,9 +19,9 @@ const Home = () => {
                     </div>
                     <div className="info">
                         <h4 ><FiMail />  bramtechxxvi@gmail.com</h4>
-                        <h6><FiPhone/>  +234-701-717-5172</h6>
-                        <h6><FiLinkedin />  my-linkedIn</h6>
-                        <h6><FiMapPin />   Lagos, Nigeria</h6>
+                        <h4><FiPhone/>  +234-701-717-5172</h4>
+                        <h4><FiLinkedin />  my-linkedIn</h4>
+                        <h4><FiMapPin />   Lagos, Nigeria</h4>
                         <h4><FiGithub />   my-gitHubLink</h4>
 
                     </div>
