c05370b0cc3eff3c5686b56f63871a3437ebf1b6
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-05 16:23:58 +0100

    update

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const HeroSection = () => {
+    const roles = ["Back-End Engineer", "Software Engineer"];
+    const typedText = document
+  return (
+    <div>HeroSection</div>
+  )
+}
