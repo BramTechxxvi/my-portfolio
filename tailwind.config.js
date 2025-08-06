cdb2ba305c9e6a8b987107242c370573014ab422
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-06 09:58:33 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx}",
+    "./src/assets/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
