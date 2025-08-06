cded338d83fd33cc701bd4a4c2ac1255ac860fd3
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-06 09:53:50 +0100

    new

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 
-export const HeroSection = () => {
+const HeroSection = () => {
     const roles = ["Back-End Engineer", "Software Engineer"];
-    const typedText = document
+    const [roleIndex, setRoleIndex] = useState(0);
+    const [charIndex, setCharIndex] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState("")
+
+    useEffect(()=> {
+        const currentRole = roles[roleIndex];
+        const visibleText = currentRole.subString(0, charIndex)
+        setText(visibleText)
+
+        let typingSpeed = isDeleting ? 60 : 100;
+
+        const timeout = setTimeout(()=> {
+            if(!isDeleting && charIndex  < currentRole.length) {
+                setCharIndex((prev)=> prev +1);
+            } else if (isDeleting && charIndex > 0) {
+                setCharIndex((prev)=> prev - 1);
+                setRoleIndex((prev)=> (prev + 1) % roles.length);
+            } else {
+                if(!isDeleting) {
+                    setIsDeleting(true);
+                } else {
+                    setIsDeleting(false);
+                    setRoleIndex((prev) => (prev + 1) % roles.length);
+                }
+            }
+        }, typingSpeed);
+        return () => clearTimeout(timeout);
+    }, [charIndex, isDeleting, roleIndex]);
+
   return (
     <div>HeroSection</div>
   )
