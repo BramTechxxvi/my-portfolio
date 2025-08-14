b2dfde535b0d1473185830c285466c0afe8127db
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-14 17:07:37 +0100

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,5 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { FiMail } from "react-icons/fi";
-import { FiPhone } from "react-icons/fi";
-import { FiLinkedin } from "react-icons/fi";
-import { FiGithub } from "react-icons/fi";
-import { FiMapPin } from "react-icons/fi";
 import "./Home.css"
 import HeroSection from "./components/HeroSection";
 
