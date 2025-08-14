730d326344b9602bfd3f815ae90291a60450d05c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-14 17:49:09 +0100

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom";
-
+import { Outlet, useLocation } from "react-router-dom";
+import Header from './Header';
+import Footer from './Footer';
 
 const Layout = () => {
     const location = useLocation();
     const isHome = location.pathname === '/';
-    
+
   return (
-    <div>Layout</div>
+    <>
+    <Header isHome={isHome} />
+    <main>
+        <Outlet />
+    </main>
+    </>
   )
 }
