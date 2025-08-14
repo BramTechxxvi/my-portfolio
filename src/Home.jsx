61d1bb667cab4d99bc810879c5f479f32b54812c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-14 17:40:53 +0100

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,12 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import "./Home.css"
 import HeroSection from "./components/HeroSection";
 
 const Home = () => {
     return (
         <div className="Home">
-            <Header />
             <main className="mainWrapper">
                 <HeroSection />
             </main>
-            <Footer />
         </div>
     );
 }
