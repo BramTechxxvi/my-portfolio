103e150e84264ad1c0d3204fdeac9fcb225c7634
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-18 09:31:42 +0100

    update

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -12,7 +12,14 @@ const BottomNav = () => {
         { to: "/", icon: <FiHome />, label: "Contact" },      
     ]
     return (
-        <nav className='fixed bottom-6 left-1/2 transform -translate-x-1/2 bg-white shadow-md rounded-full px-6 py-3 gap-6 z-50'></ nav>    
+        <nav className='fixed bottom-6 left-1/2 transform -translate-x-1/2 bg-white shadow-md rounded-full px-6 py-3 gap-6 z-50'>
+            {navItems.map((item)=> {
+                <Link
+                key={item.to}
+                to={item.to}
+                className={`flex flex-col items-center`}></Link>
+            })}
+        </ nav>    
     );
 };
 
