ab87894c3dc62b336285c07d3a629bee1d8595c6
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-20 18:30:38 +0100

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,18 @@
 import HeroSection from "./components/HeroSection";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 import Contact from "./components/Contact";
-import BottomNav from "./components/BottomNav";
 
 const Home = () => {
     return (
-        <div className="flex flex-col min-h-screen">
-            <main className="flex-1">
-                <HeroSection/>
+        <div className="flex flex-col min-h-screen w-full">
+            <main className="flex-1 w-full">
+                <section id="hero"><HeroSection /></section>
+                <section id="about"><About /></section>
+                <section id="projects"><Projects /></section>
+                <section id="skills"><Skills /></section>
+                <section id="contact"><Contact /></section>
             </main>
         </div>
     );
