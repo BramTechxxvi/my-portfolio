69fd314ed143c43e84badde7abc724cbf5715648
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-20 18:14:24 +0100

    m

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = ({ isHome })=> {
     return (
         <footer 
-        className={`${ isHome? `bg-gray-900 text-gray-200` : `bg-gray-100 text-black`} text-center py-4`}
+    className={`${ isHome? `bg-gray-900 text-gray-200` : `bg-gray-100 text-black`} text-center px-8 py-5 sm:px-12 sm:py-7`}
         >
             <p>&copy; {new Date().getFullYear()} Ibrahim Ibrahim</p>
         </footer>
