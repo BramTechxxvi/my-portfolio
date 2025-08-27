800f114fd384d11c53d8f3b777aa6a2e9c1e9ff4
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-27 15:54:34 +0100

    update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const NotFound = () => {
   return (
     <div>
-      <h1> 
-        className="text-center mt-20 text-2xl">Page Not Found</h1>
+      <h1 className="text-center mt-20 text-2xl">
+        Page Not Found
+      </h1>
     </div>
   )
 }
+
+export default NotFound
