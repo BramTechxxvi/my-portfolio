460d5adc3d3d0fa19d49a9560d8e66ce665dfcf9
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-27 16:17:58 +0100

    update

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,8 +1,19 @@
-import {useState} from 'react'; '4.2k (gripped: 1.8k)'
+import {useEffect, useState} from 'react'; '4.2k (gripped: 1.8k)'
 import {Sun, Moon } from "lucide-react"; '2k (gripped: 1.8k)'
 
 const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme");
+        if(storedTheme === "dark") {
+            setIsDarkMode(true);
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            setIsDarkMode(false);
+        }
+    });
 
     const toggleTheme = () => {
         if(isDarkMode) {
