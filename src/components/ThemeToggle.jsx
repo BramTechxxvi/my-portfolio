dc3be3f9ece90a0efe34e6e1aac99f972c3e5c1e
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-27 15:34:44 +0100

    update

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -4,5 +4,8 @@ import {Sun} from "lucide-react";
 const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
 
-  return <button>{ isDarkMode ? <Sun /> }</button>
-}
+  return <button>{ isDarkMode ? <Sun /> : <Monn />}</button>;
+
+};
+
+export default ThemeToggle;
