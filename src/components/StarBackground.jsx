ecc71e8e0a09776f9be1d32aaf46e651a81cd36c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-28 15:10:10 +0100

    new

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -1,13 +1,33 @@
-import { useState} from 'react'; '4.2k (gzipped: 1.8k)'
+import { useEffect, useState} from 'react'; '4.2k (gzipped: 1.8k)'
 
 const StarBackground = () => {
     const [stars, setStars] = useState([])
 
+    useEffect(() => {
+        generateStars();
+    }, []);
+
     const generateStars = () => {
-        const numberOfStars = Math.fl
+        const numberOfStars = Math.floor(
+            {window.innerWidth * window.innerHeight} / 10000
+        );
+        const newStars = [];
+        for (let i = 0; i < numberOfStars; i++) {
+            newStars.push({
+                id: i,
+                size: Math.random() * 3 + 1,
+                x: Math.random() * 100,
+                y: Math.random() * 100,
+                opacity: Math.random() * 0.5 + 0.5,
+                animationDuration: Math.random() * 4 + 2,
+            });
+        }
+        setStars(newStars);
+    };
   return (
     <div>StarBackground</div>
   )
+
 }
 
 export { StarBackground }
