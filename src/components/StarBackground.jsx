2c1bac1b0ca95014ed155e25e23ff50daa0cae5c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-28 14:49:00 +0100

    update

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -2,9 +2,9 @@ import { useState} from 'react'; '4.2k (gzipped: 1.8k)'
 
 const StarBackground = () => {
     const [stars, setStars] = useState([])
-    
+
     const generateStars = () => {
-        const 
+        const numberOfStars = Math.fl
   return (
     <div>StarBackground</div>
   )
