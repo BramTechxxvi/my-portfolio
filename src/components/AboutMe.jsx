bf14313e76bbfc06f0da2e827a46d5eebe846380
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-30 19:42:34 +0100

    update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,8 +4,14 @@ const AboutMe = ()=> {
         id="about"
         className="py-24 px-4 relative">
             {" "}
-            <div>
-                
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+                    About <span className="text-primary"></span>
+                </h2>
+
+                <div>
+                    
+                </div>
             </div>
         </section>
     )
