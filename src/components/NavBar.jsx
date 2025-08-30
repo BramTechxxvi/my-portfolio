68c1080d1116f42e0f1ec0883e7699eaedc0c3a1
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-30 09:48:40 +0100

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,14 +26,37 @@ const NavBar = () => {
         isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
         )}>
         <div className='container flex items-center justify-between'>
-            <a className='text-xl font-bold text-primary flex items-center'>
+            <a
+            className='text-xl font-bold text-primary flex items-center'
+            href="#hero">
                 <span className='relative z-10'>
-                    <span className='text-glow text-foregroung'></span>
+                    <span className='text-glow text-foreground'>Ibrahim Ibrahim</span>{" "}
+                    Babatunde
                 </span>
             </a>
-        </div> 
-            
 
+            <div className='hidden md:flex space-x-8'>
+                {navItems.map((item, key)=> (
+                    <a key={key} href={item.href}
+                    className='text-foreground/80 hover:text-primary transition-colors duration-300'>
+                        {item.name}
+                    </a>
+                ))}
+            </div>
+
+            <div className={cn(
+                ""
+                )}>
+                <div className='hidden md:flex space-x-8'>
+                    {navItems.map((item, key)=> (
+                        <a key={key} href={item.href}
+                        className='text-foreground/80 hover:text-primary transition-colors duration-300'>
+                        {item.name}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </div> 
     </nav>
   )
 }
