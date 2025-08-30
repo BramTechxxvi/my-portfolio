4380483699f2caf81e22235d1e3f3b3781021f6c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-30 18:37:44 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import  ThemeToggle  from "../components/ThemeToggle";
 import StarBackground from "../components/StarBackground";
-//  
+import NavBar from "../components/NavBar";
+import HeroSection from "../components/HeroSection";
 const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         <ThemeToggle />
         <StarBackground />
         <NavBar />
+        <main>
+          <HeroSection />
+        </main>
     </div>
   )
 }
