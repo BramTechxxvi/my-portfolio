460dbd65d7f0ed310ee39eafd393d03f1cd2cacd
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-30 19:23:27 +0100

    update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,11 +5,39 @@ const HeroSection = () => {
     <section 
     id="hero"
     className='relative min-h-screen flex flex-col items-center justify-center px-4'>
-        HeroSection
         <div className='container max-w-4xl mx-auto text-center z-10'>
+            
             <div className='space-y-6'>
-                <span>Hi</span>
+                <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                    <span className='opacity-0 animate-fade-in'>Hi👋, I'm </span>
+                    <span className='text-primary opacity-0 animate-fade-in-delay-1'>
+                        {" "}
+                        Ibrahim
+                    </span>
+                    <span className='text-gradient ml-2 opacity-0 animate-fade-in-delay-2'>
+                        {" "}
+                        Ibrahim
+                    </span>
+                </h1>
+
+                <p className='text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3'>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse architecto possimus voluptatem 
+                    maxime quis quaerat exercitationem dolores ab iure! Magni sit necessitatibus dolores dolore 
+                    corporis enim quo quia eligendi rerum.
+                </p>
+
+                <div className='pt-4 opacity-0 animate-fade-in-delay-4'>
+                    <a href="#projects"
+                    className='cosmic-button'>
+                        View My Work
+                    </a>
+                </div>
             </div>
+        </div>
+
+        <div 
+        className='absolute buttom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce'> className=""
+
         </div>
     </section>
   )
