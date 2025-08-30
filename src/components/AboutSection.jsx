3be1ab4c9ee937b3fa599a070fe5d51f9ec59e11
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-30 20:46:34 +0100

    update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,24 +30,45 @@ const AboutSection = ()=> {
                         </p>
 
                         <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
-                        <a href="#contact" className="cosmic-button">
-                            {" "}
-                            Get In Touch
-                        </a>
+                            <a href="#contact" className="cosmic-button">
+                                {" "}
+                                Get In Touch
+                            </a>
 
-                        <a href="" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
-                            {" "}
-                            Download CV
-                        </a>
+                            <a href="" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                                {" "}
+                                Download CV
+                            </a>
+                        </div>
                     </div>
+
+                    <div className=" grid grid-cols-1 gap-6">
+                        <div className=" gradient-border p-6 card-hover">
+                            <div className="flex items-start gap-4"> 
+                                <div className="p-3 rounded-full bg-primary/10">
+                                <Code className="h-6 w-6 text-primary"/>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className=" gradient-border p-6 card-hover">
+                            <div className="flex items-start gap-4"> 
+                                <div className="p-3 rounded-full bg-primary/10">
+                                <Code className="h-6 w-6 text-primary"/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className=" gradient-border p-6 card-hover">
+                            <div className="flex items-start gap-4"> 
+                                <div className="p-3 rounded-full bg-primary/10">
+                                <Code className="h-6 w-6 text-primary"/>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
-
-                    
-
-                    
-
-                    
-
                 </div>
             </div>
 
