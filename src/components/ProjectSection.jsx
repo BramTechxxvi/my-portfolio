8f0cb0da04c61e61fb0e7f5bd6c9c55884dc705c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-08 17:23:56 +0100

    update

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 
 const projects = [
     {
-        
+        id: 1,
+        title: "Project One",
+        description: "A brief description of Project One.",
+        imageUrl: "https://via.placeholder.com/300",
+        githubUrl: "#"
+    },
+    {
+        id: 2,
+        title: "Project One",
+        description: "A brief description of Project One.",
+        imageUrl: "https://via.placeholder.com/300",
+        githubUrl: "#"
+    },
+    {
+        id: 3update,
+        title: "Project One",
+        description: "A brief description of Project One.",
+        imageUrl: "https://via.placeholder.com/300",
+        githubUrl: "#"
     }
 ]
 const ProjectSection = () => {
