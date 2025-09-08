00374e1c2fc1fcf8b0f65c9afc6abff8f5f48176
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-08 15:36:51 +0100

    update

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -36,8 +36,15 @@ const SkillsSection = () => {
 
   <div className="container mx-auto max-w-5xl">
     <h2 className='text-3xl md:text-4xl font-bold mb-12 text-center'>
-      My <span> Skills</span>
+      My <span className="text-primary"> Skills</span>
     </h2>
+
+    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
+      {skills.map((skill, index) => (
+        <div
+        key={index} className="bg-card p-6 rounded-lg shadow-xs card-hover"></div>
+      ))}
+    </div>
 
   </div>
   </section>
