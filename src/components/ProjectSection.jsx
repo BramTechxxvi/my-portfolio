1adc16a836384eddfed6843b86dfe23aec3c601e
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-09 16:53:27 +0100

    update

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -38,6 +38,13 @@ const ProjectSection = () => {
                 crafted with attention to detail, performance, and user experience
             </p>
 
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+                {projects.map(({project, key})=> (
+                    <div 
+                    key={key} 
+                    className='group bg-card rounded-lg overflow-hidden shadow-xs card:hover'>
+            </div>
+
         </div>
     </section>
   )
