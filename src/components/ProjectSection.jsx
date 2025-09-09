38396337b6059a8e07d1627a932b278bfe65ef83
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-09 17:10:27 +0100

    update

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -40,12 +40,16 @@ const ProjectSection = () => {
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'
             >
-                {projects.map(({project, key})=> (
+                {projects.map((project, key) => (
                     <div 
                     key={key} 
                     className='group bg-card rounded-lg overflow-hidden shadow-xs card:hover'
                     >
-                        ))}
+                        <div className='h-48 overflow-hidden'>
+                            <img src={project.imageUrl} alt={project.title} />
+                        </div>
+                    </div>
+                ))};
             </div>
 
         </div>
