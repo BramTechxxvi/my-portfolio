78fa2a29ccbde386c56f28597071511775679d0b
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-10 12:01:21 +0100

    update

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ContactSection = () => {
   return (
-    <section className='py-24 px-4 relative bg-secondary/30'>
+    <section className='py-24 px-4 relative bg-secondary/30'
+    >
+        <h2 className='text-3xl md:text-4xl font-bold mb-12 text-center'>
+            Get In <span className="text-primary"> Touch</span>
+        </h2>
 
     </section>
   )
