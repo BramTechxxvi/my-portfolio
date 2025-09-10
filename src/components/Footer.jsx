e74952cdcb11c8d609b5c87d875c327a5e37f595
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-10 16:17:25 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <footer className="w-full bg-background text-foreground text-center px-4 py-6 border-t border-border">
+    <footer className="w-full bg-background text-foreground text-center px-4 py-6">
             <p className="text-sm tracking-wide">&copy; {new Date().getFullYear()} Ibrahim Ibrahim</p>
         </footer>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import AboutSection from "../components/AboutSection";
 import SkillsSection from "../components/SkillsSection"
 import ProjectSection from "../components/ProjectSection";
 import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -19,6 +20,7 @@ const Home = () => {
           <SkillsSection />
           <ProjectSection />
           <ContactSection />
+          <Footer />
         </main>
     </div>
   )
