c55f7c222a3d68d7e2513eaa2290dbf3ec88cd52
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-10 12:03:26 +0100

    update

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -4,9 +4,11 @@ const ContactSection = () => {
   return (
     <section className='py-24 px-4 relative bg-secondary/30'
     >
-        <h2 className='text-3xl md:text-4xl font-bold mb-12 text-center'>
-            Get In <span className="text-primary"> Touch</span>
-        </h2>
+        <div>
+            <h2 className='text-3xl md:text-4xl font-bold mb-4 text-center'>
+                Get In <span className="text-primary"> Touch</span>
+            </h2>
+        </div>
 
     </section>
   )
