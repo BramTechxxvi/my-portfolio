2a8ef0c5c8e4ac10f64886fed054d6598b0b1d16
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-10 16:39:11 +0100

    uodate

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -37,8 +37,8 @@ const HeroSection = () => {
         </div>
 
         <div 
-        className='absolute buttom-8 left-1/2 transform-translate-x-1/2 flex flex-col items-center animate-bounce'>
-            <span className='text-sm text-muted-foreground mb:2'>Scroll</span>
+        className='absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center animate-bounce'>
+            <span className='text-sm text-muted-foreground mb-2'>Scroll</span>
             <ArrowDown className="h-5 w-5 text-primary" />
         </div>
     </section>
