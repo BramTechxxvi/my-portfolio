3cf442ec03ed783b4f6beb930004aff3479ad56c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-10 12:07:05 +0100

    update

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -4,10 +4,15 @@ const ContactSection = () => {
   return (
     <section className='py-24 px-4 relative bg-secondary/30'
     >
-        <div>
+        <div className='container max-w-5xl mx-auto'>
             <h2 className='text-3xl md:text-4xl font-bold mb-4 text-center'>
                 Get In <span className="text-primary"> Touch</span>
             </h2>
+
+            <p className='text-center text-muted-foreground mb-12 max-w-2xl mx-auto'>
+                Have a project in mind or want to collaborate? Feel freetp reach out.
+                I'm always open to discussing new opportunities.
+            </p>
         </div>
 
     </section>
