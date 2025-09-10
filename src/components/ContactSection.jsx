23e985abf0e57b86f2dc08b7ab57b7a1c2419d07
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-10 16:04:19 +0100

    update

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -71,7 +71,7 @@ const ContactSection = () => {
                     <div className='pt-8'>
                         <h4 className='font-medium mb-4'>Connect With Me</h4>
                         <div className="flex space-x-4 justify-center">
-                            <a href="#" target='_blank'>
+                            <a href="linkedin.com/in/ibrahim-ibrahim-1001b333a" target='_blank'>
                                 <Linkedin />
                             </a>
                             <a href="#" target='_blank'>
