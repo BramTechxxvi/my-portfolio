9e80306a0e1a981242baea0a6d1363efc614c84e
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-10 15:19:25 +0100

    update

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -143,7 +143,7 @@ const ContactSection = () => {
 
                     <button
                     type="submit"
-                    className=''
+                    className={cn("cosmic-button w-full flex justify-center items-center",)}
                     >
 
                     </button>
