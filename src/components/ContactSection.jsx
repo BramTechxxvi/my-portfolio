eb8cb6383816050f157510d993a72c453e34485a
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-11 16:29:01 +0100

    update

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -124,7 +124,7 @@ const ContactSection = () => {
                     <h3 className='text-2xl font-semibold mb-6'>Send a Message</h3>
 
 
-                    <form className='space-y-6'>
+                    <form ref={formRef} onSubmit={sendEmail} className='space-y-6'>
                     <div>
                         <label htmlFor="name"
                         className='block text-sm font-medium mb-2'>
