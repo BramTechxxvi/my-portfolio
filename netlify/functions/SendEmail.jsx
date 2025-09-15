cd5b76893837b62d4b749ae5f77f904f5bb7999b
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-15 15:59:01 +0100

    update

--- netlify/functions/SendEmail.jsx
+++ netlify/functions/SendEmail.jsx
@@ -1,5 +1,9 @@
 
 
 export async function handler(event, context) {
-    
+    if(event.httpMethod !== "POST") {
+        return {
+            statusCode: 405,
+            body: JSON.stringify({})
+        }
 }
