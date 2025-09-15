59398ba62aaf31c6d27e9fe86b4113b8d098a0aa
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-15 17:03:41 +0100

    update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -39,7 +39,7 @@ const HeroSection = () => {
                     <img 
                         src="/usage.JPG" 
                         alt="Ibrahim" 
-                        className="w-60 h-60 md:w-96 md:h-96 object-cover rounded-full shadow-lg opacity-0 animate-fade-in-delay-2" 
+                        className="w-60 h-60 md:w-96 md:h-96 object-cover rounded-full shadow-lg opacity-0 animate-fade-in-delay-2 transform transition duration-700 hover:scale-105" 
                     />
                 </div>
             </div>
