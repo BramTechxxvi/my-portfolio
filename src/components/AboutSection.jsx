e2939d37466ee990e62a7a2a9290289cb15f65f3
BramTechxxvi <bramtechxxvi@gmail.com>
2025-09-16 00:43:35 +0100

    upd

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -14,7 +14,7 @@ const AboutSection = ()=> {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
                     <div className="space-y-6">
                         <h3 className="text-2xl font-semibold">
-                            Passionate Web Beveloper & Tech Creator
+                            Backend Engineer Web Beveloper & Tech Creator
                         </h3>
 
                         <p 
